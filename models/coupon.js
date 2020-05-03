44535bcefb31e6f56372161b5f40945dca8a5ddb
mgojak1 <mgojak1@etf.unsa.ba>
2020-05-03 14:00:27 +0200

    Coupon model added

---
+++ models/coupon.js
@@ -0,0 +1,22 @@
+import Sequelize from "sequelize";
+
+module.exports = function (sequelize) {
+    const Coupon = sequelize.define(
+        "coupon",
+        {
+            id: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                unique: true,
+                primaryKey: true,
+                autoIncrement: true,
+            },
+            expiryDate: Sequelize.DATE,
+            code: Sequelize.STRING,
+            discount: Sequelize.REAL,
+            used: Sequelize.BOOLEAN,
+        },
+        { freezeTableName: true, timestamps: false }
+    );
+    return Coupon;
+};
