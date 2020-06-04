1305a6bc399add576569c0a569839e663fbd5861
jcaluk1 <jcaluk1@etf.unsa.ba>
2020-06-04 14:21:12 +0200

    added mock data

--- initialize.js
+++ initialize.js
@@ -19,29 +19,43 @@ function intialize() {
     const users = [
         {
             id: 1,
-            name: "admin",
-            lastName: "adminic",
-            email: "email1@gmail.com",
+            name: "ADMIN1",
+            lastName: "admin1",
+            email: "admin1@gmail.com",
             address: "address1",
             username: "admin1",
             password: "$2b$10$Opn1pnyxtr7XbRpGoyf2GuowuBP0dvYD8eQdJJ./ug8cDYYaCcFcS", //password
             role: 1,
         },
+
         {
             id: 2,
-            name: "employee",
-            lastName: "employeeic",
-            email: "email2@gmail.com",
+            name: "ADMIN2",
+            lastName: "admini2",
+            email: "admin2@gmail.com",
+            address: "address2",
+            username: "admin2",
+            password: "$2b$10$Opn1pnyxtr7XbRpGoyf2GuowuBP0dvYD8eQdJJ./ug8cDYYaCcFcS", //password
+            role: 1,
+        },
+
+
+        {
+            id: 3,
+            name: "employee1",
+            lastName: "employee1",
+            email: "employee1@gmail.com",
             address: "address2",
             username: "employee1",
             password: "$2b$10$Opn1pnyxtr7XbRpGoyf2GuowuBP0dvYD8eQdJJ./ug8cDYYaCcFcS", //password
             role: 2,
         },
+
         {
-            id: 3,
-            name: "client",
-            lastName: "clientic",
-            email: "email3@gmail.com",
+            id: 4,
+            name: "client1",
+            lastName: "client1",
+            email: "client1@gmail.com",
             address: "address3",
             username: "client",
             password: "$2b$10$Opn1pnyxtr7XbRpGoyf2GuowuBP0dvYD8eQdJJ./ug8cDYYaCcFcS",
@@ -51,64 +65,138 @@ function intialize() {
     const categories = [
         {
             id: 1,
-            name: "category1",
-            description: "description1",
+            name: "Slana jela",
+            description: "Slana jela, jela sa rostilja ...",
         },
         {
             id: 2,
-            name: "category2",
-            description: "description2",
+            name: "Slatka jela",
+            description: "Deserti, kolači ...",
         },
+        {
+            id: 3,
+            name: "Predjela",
+            description: "Supe, hladne plate, omleti ...",
+        },
+        {
+            id: 4,
+            name: "Bezalkoholna pića",
+            description: "Razne vrste sokova i drugih bezalkoholnih pića ...",
+        },
+        {
+            id: 5,
+            name: "Alkoholna pića",
+            description: "Razne vrste sokova i drugih bezalkoholnih pića ...",
+        },
+
     ];
     const products = [
         {
             id: 1,
-            name: "product1",
-            description: "description1",
+            name: "Ćevapi - Mala porcija",
+            description: "5 ćevapa u pola lepine",
             categoryId: 1,
-            price: 1,
+            price: 3,
         },
         {
             id: 2,
-            name: "product2",
-            description: "description2",
+            name: "Ćevapi - Velika porcija",
+            description: "10 ćevapa u cijelu lepinu",
+            categoryId: 1,
+            price: 6,
+        },
+        {
+            id: 3,
+            name: "Voćna salata",
+            description: "Voćna salata, ima bruku voća, ekstra zdrava",
             categoryId: 2,
+            price: 3,
+        },
+        {
+            id: 4,
+            name: "Palačinci",
+            description: "2 palačinka po želji eurokrem/džem/pašteta",
+            categoryId: 2,
+            price: 4,
+        },
+        {
+            id: 5,
+            name: "Suho meso sa sirom",
+            description: "Suho meso sa sirom",
+            categoryId: 3,
+            price: 6,
+        },
+        {
+            id: 6,
+            name: "Omlet",
+            description: "Dobar omlet pravo a nije skup",
+            categoryId: 3,
+            price: 1,
+        },
+        {
+            id: 7,
+            name: "Kafa - mala",
+            description: "Mala kafa uz dodatak mlijeka po izboru",
+            categoryId: 4,
+            price: 1.5,
+        },
+        {
+            id: 8,
+            name: "Coca - cola",
+            description: "A šta se ima govorit o koli",
+            categoryId: 4,
             price: 2,
         },
+        {
+            id: 9,
+            name: "Sarajevsko pivo",
+            description: "Domaće pibo klasika",
+            categoryId: 5,
+            price: 2,
+        },
+        {
+            id: 10,
+            name: "Viski",
+            description: "Viski mjera 0.03",
+            categoryId: 5,
+            price: 3,
+        },
+
+
     ];
 
     const coupons = [
         {
             id: 1,
-            code: "coupon1",
+            code: "111111",
             expiryDate: Date.now(),
             discount: 0.1,
             used: false,
         },
         {
             id: 2,
-            code: "coupon2",
+            code: "222222",
             expiryDate: Date.now(),
             discount: 0.2,
             used: false,
         },
         {
             id: 3,
-            code: "coupon3",
+            code: "333333",
             expiryDate: Date.now(),
             discount: 0.3,
             used: false,
         },
         {
             id: 4,
-            code: "coupon4",
+            code: "444444",
             expiryDate: Date.now(),
             discount: 0.4,
             used: false,
         },
         {
             id: 5,
-            code: "coupon5",
+            code: "555555",
             expiryDate: Date.now(),
             discount: 0.5,
             used: false,
@@ -116,9 +204,10 @@ function intialize() {
     ];
 
     const orderItems = [
-        { id: 1, quantity: 1, productId: 1, orderId: 1 },
-        { id: 2, quantity: 2, productId: 2, orderId: 2 },
-        { id: 3, quantity: 2, productId: 2, orderId: 2 },
+        { id: 1, quantity: 2, productId: 2, orderId: 1 },
+        { id: 2, quantity: 2, productId: 8, orderId: 1 },
+        { id: 3, quantity: 1, productId: 4, orderId: 2 },
+        { id: 4, quantity: 1, productId: 9, orderId: 2 },
     ];
 
     const orders = [
@@ -133,8 +222,8 @@ function intialize() {
             id: 2,
             orderDate: Date.now(),
             completed: false,
-            userId: 2,
-            couponId: 2,
+            userId: 3,
+            couponId: 3,
         },
     ];
 
